ab256bdfaac2a12e1a75780daf8e6e39e20b5525
haibui <hoanghaibui2003@gmail.com>
2019-05-25 14:38:44 +0800

    Correct replace

--- libs/replace-classes.js
+++ libs/replace-classes.js
@@ -8,46 +8,49 @@ function setClassesTable(table) {
 	classesTable = table;
 }
 
+function isSkipCase(text) {
+	//Todo should log somewhere for case to convert this case later
+	let result = false;
+	let casesSkip = ["{{"];
+	_.each(casesSkip, c => {
+		if (text.indexOf(c) > -1) {
+			result = true;
+			return false;
+		}
+	});
+	return result;
+}
+
 function replaceText(text) {
 	let orginal = text;
-	let reget = new RegExp("class=\".*?\"", "gm");
+	let reget = new RegExp("class=\".*?\"", "gi");
 	let matches;
+	let replaces = [];
 	while ((matches = reget.exec(text))) {
 		let classesName = orginal = (matches[0]);
-		 // console.log("before",classesName);
+		if(isSkipCase(classesName)){
+			continue;
+		}
+		// console.log("before", classesName);
 		_.each(classesTable, row => {
-			classesName = classesName.replace(new RegExp(row[0], "g"), row[1]);
+			classesName = classesName.replace(row[0], row[1]);
 			// if(orginal !== text){
 			// 	console.log(row);
 			// 	orginal = text;
 			// }
 		});
-		 // console.log("after",classesName);
-		text = text.replace(orginal,classesName);
+		// console.log("after", classesName);
+		replaces.push([orginal, classesName]);
 	}
+	// console.log("replaces", replaces.length);
+	_.each(replaces, place => {
+		// console.log(place);
+		text = text.replace(place[0], place[1]);
+	});
 
 	return text;
 }
-function replaceText2(text) {
-	let orginal = text;
-	let reget = new RegExp("class=\".*\"", "gm");
-	let matches;
-	while ((matches = reget.exec(text))) {
-		let classesName = orginal = (matches[0]);
-		// console.log("before",classesName);
-		_.each(classesTable, row => {
-			classesName = classesName.replace(new RegExp(row[0], "g"), row[1]);
-			// if(orginal !== text){
-			// 	console.log(row);
-			// 	orginal = text;
-			// }
-		});
-		// console.log("after",classesName);
-		text = text.replace(orginal,classesName);
-	}
 
-	return text;
-}
 function replaceText1(text) {
 	_.each(classesTable, row => {
 		text = text.replace(new RegExp(row[0], "g"), row[1]);
@@ -62,6 +65,7 @@ function replaceText1(text) {
 
 const revealed = {
 	setClassesTable,
-	replaceText
+	replaceText,
+	isSkipCase
 };
 module.exports = revealed;
